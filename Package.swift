4e8c2d4bedf8c09197f146adfb084b0bee51ab26
Ali Donyaie <ali@nativeblocks.io>
2024-09-17 12:35:00 +0330

    limit package platforms to macOS and iOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 
 let package = Package(
     name: "NativeblocksCompiler",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         .library(name: "NativeblocksCompiler", targets: ["NativeblocksCompiler"]),
         .plugin(name: "GenerateProvider", targets: ["GenerateProvider"]),
